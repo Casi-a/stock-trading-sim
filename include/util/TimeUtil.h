efcdfec929af27e918f62aa2e220612bc33b42ab
Bada <1127bada@gmail.com>
2025-12-01 15:36:01 +0900

    refactor: TimeUtil 헤더 분리

--- include/util/TimeUtil.h
+++ include/util/TimeUtil.h
@@ -1,14 +1,12 @@
 #pragma once
 #include <string>
 
+// 시간 및 날짜 관련 유틸리티 클래스
 class TimeUtil {
 public:
-    // 현재 날짜 반환 (YYYY-MM-DD)
-    static std::string getToday();
-    
-    // 현재 시간 반환 (HH:MM:SS)
-    static std::string getNow();
+    // 현재 시스템 시간 반환 (HH:MM:SS)
+    static std::string now();
 
-    // 시간 흐름 처리 (Application 루프에서 호출)
-    static void advanceTime();
+    // 오늘 날짜 반환 (YY-MM-DD)
+    static std::string todayDate();
 };
