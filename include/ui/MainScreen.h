8a8286f9b0ede5793027b34ccfc63ba3bc0b2d04
Bada <1127bada@gmail.com>
2025-12-01 15:36:01 +0900

    refactor: MainScreen 헤더 분리

--- include/ui/MainScreen.h
+++ include/ui/MainScreen.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "Screen.h"
 
+// 메인 메뉴 화면
 class MainScreen : public Screen {
 public:
     MainScreen(Application* app) : Screen(app) {}
-    
-    // [Logic] 메인 메뉴 출력 및 화면 전환 분기 처리
+
+    // 메인 메뉴 출력 및 화면 전환 입력 처리
     virtual void show() override;
 };
