fe33ad302084e22ab0db57a3d64672a480b2d8ac
Bada <1127bada@gmail.com>
2025-12-01 15:36:16 +0900

    chore: src 폴더 구버전 파일 삭제

--- src/model/Order.cpp
+++
@@ -1,6 +0,0 @@
-#include "../../include/model/Order.h"
-
-// 생성자 구현
-Order::Order(std::string sym, bool buy, int qty, double p, std::string d)
-    : symbol(sym), isBuy(buy), quantity(qty), price(p), date(d) {
-}
